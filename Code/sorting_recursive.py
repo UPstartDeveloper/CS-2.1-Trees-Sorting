3f4d63264e38e244bbbd82eabf63208f223ef3d6
= <zainr7989@gmail.com>
2020-04-04 13:51:20 -0400

    Accomplish merge sort, still needs to mutate input.

--- Code/sorting_recursive.py
+++ Code/sorting_recursive.py
@@ -1,4 +1,5 @@
 #!python
+from sorting_iterative import insertion_sort
 
 
 def merge(items1, items2):
@@ -8,14 +9,34 @@ def merge(items1, items2):
     TODO: Memory usage: ??? Why and under what conditions?"""
     # make a new list
     merged = list()
-    # append to merged - choose between the first elements in smaller arrays
-    for i in range(len(items1) + len(items2)):
-        if items1[0] < items2[0]:
-            merged.append(items1.pop(0))
+    # track idices in both lists of items
+    index1, index2 = 0, 0
+    # sequence the elements from both items lists
+    while index1 < len(items1) and index2 < len(items2):
+        # grab items from both items lists to compare
+        value1 = items1[index1]
+        value2 = items2[index2]
+        # decide which is lesser
+        if value1 < value2:
+            merged.append(value1)
+            index1 += 1
         else:
-            merged.append(items2.pop(0))
+            merged.append(value2)
+            index2 += 1
+    # if the halves weren't eqaully sized exactly, then add all remaining here
+    if index1 < len(items1):
+        merged.extend(items1[index1:])
+    else:
+        merged.extend(items2[index2:])
     # return the new list
     return merged
+
+
+def split(items):
+    '''Splits given list of items into 2 equal halves, and returns them.'''
+    mid_index = len(items) // 2
+    # return the left and right in a tuple
+    return items[:mid_index], items[mid_index:]
 
 
 def split_sort_merge(items):
@@ -24,20 +45,13 @@ def split_sort_merge(items):
     a list in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Split items list into approximately equal halves
-    '''
-    1) calculate the middle index
-    2) setting vars for left-right halves
-    3) repeat 1-2 until lists are len(list) == 2
-    '''
-    # TODO: Sort each half using any other sorting algorithm
-    '''
-    4) insertion sort each of the smaller
-    '''
-    # TODO: Merge sorted halves into one list in sorted order
-    '''
-    5) call merge
-    '''
+    # Split items list into approximately equal halves
+    left, right = split(items)
+    # Sort each half using any other sorting algorithm
+    insertion_sort(left)
+    insertion_sort(right)
+    # Merge sorted halves into one list in sorted order
+    return merge(left, right)
 
 
 def merge_sort(items):
@@ -45,10 +59,31 @@ def merge_sort(items):
     sorting each recursively, and merging results into a list in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check if list is so small it's already sorted (base case)
-    # TODO: Split items list into approximately equal halves
+    # Check if list is so small it's already sorted (base case)
+    if len(items) == 1 or len(items) == 0:
+        return items
+    else:
+        # TODO: Split items list into approximately equal halves
+        left, right = split(items)
+        sorted_left = merge_sort(left)
+        sorted_right = merge_sort(right)
+        return merge(sorted_left, sorted_right)
+        '''merged_items = merge(sorted_left, sorted_right)
+        print(f"Merged items: {merged_items}")
+        items = merged_items
+        return items'''
+    '''items = split_sort_merge(items)
+    return merge_sort(items)'''
+    '''
+    left, right = split(items)
     # TODO: Sort each half by recursively calling merge sort
+    sorted_left = split_sort_merge(left)
+    sorted_right = split_sort_merge(right)
+    print(f"Left: {sorted_left}")
+    print(f"Right: {sorted_right}")
     # TODO: Merge sorted halves into one list in sorted order
+    return merge(sorted_left, sorted_right)
+    '''
 
 
 def partition(items, low, high):
