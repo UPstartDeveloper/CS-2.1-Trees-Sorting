59f1af59ba994d48cf925dfccb1eb4cb8e7eae15
= <zainr7989@gmail.com>
2020-04-16 15:46:09 -0400

    Annotate complexity of counting sort.

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -25,22 +25,47 @@ def find_min(numbers):
 
 def counting_sort(numbers):
     """Sort given numbers (integers) by counting occurrences of each number,
-    then looping over counts and copying that many numbers into output list.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+       then looping over counts and copying that many numbers into output list.
+
+       Running time: O(n + range), where n is the size of the number list, and
+                     range is the difference between the greatest and least
+                     values in the distribution of numbers.
+
+                     The runtime of this method scales asymptotically both with
+                     respect to the the size of n, and range. For example, in
+                     the best case the range of the distribution is not greater
+                     than its number of values. In all scenarios we need to
+                     spend time making a histogram of the numbers array (O(n)),
+                     but when the range is low compared to n, we will not have
+                     to spend as much time copying the sorted order of values
+                     into numbers. In the worst case scenario, the range is
+                     much greater than n - so then we have to combine the
+                     linear complexities of both O(n) and O(range), because
+                     making the histogram and sorting the original array occur
+                     in separate steps.
+
+       Memory usage: O(n + range), because the space required to execute this
+                     implementation of counting sort asymptotically scales with
+                     linear proportion to the size of the counts array we must
+                     construct. The factors that impact how the counts array
+                     is populated will depend on how large the numbers array
+                     is, as well as how many duplicates it contains (which
+                     tends to be inversely proportional to the range).
+
+    """
     # Find range of given numbers (minimum and maximum integer values)
-    maximum, minimum = find_max(numbers), find_min(numbers)
+    maximum, minimum = find_max(numbers), find_min(numbers)  # O(2n)
     # Create list of counts with a slot for each number in input range
     # using range to reduce the number of indices needed at front of counts
-    offset = maximum - minimum
-    counts = [0 for _ in range(offset + 1)]
+    offset = maximum - minimum  # O(1)
+    counts = [0 for _ in range(offset + 1)]  # O(range + 1)
     # Loop over given numbers and increment each number's count
-    for number in numbers:
+    for number in numbers:  # O(n)
         counts[number - minimum] += 1
-    # Loop over counts and overwirtes the input list
+    # Loop over counts and overwrites the input list
     numbers_index = 0
-    for number, count in enumerate(counts):
-        for j in range(count):
+    for number, count in enumerate(counts):  # O(range)
+        for j in range(count):  # O(?) - depends on the spread of distribution
             # use min to write the number, not the count into numbers list
             numbers[numbers_index + j] = number + minimum
         numbers_index += count
@@ -94,32 +119,26 @@ def bucket_sort(numbers, num_buckets=10):
     then sorting each bucket and concatenating all buckets in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum values)
+    # Find range of given numbers (minimum and maximum values)
     maximum, minimum, number_amt = (
         find_max(numbers),
         find_min(numbers),
         len(numbers)
     )
-    # range = maximum - minimum
-    # TODO: Create list of buckets to store numbers in subranges of input range
+    # Create list of buckets to store numbers in subranges of input range
     num_buckets = number_amt
     buckets = [LinkedList() for _ in range(num_buckets)]
-    # TODO: Loop over given numbers and place each item in appropriate bucket
+    # Loop over given numbers and place each item in appropriate bucket
     for index, number in enumerate(numbers):
-        # bucket_index = calculate_bucket_index(numbers, number, maximum)
         bucket_index = (number * number_amt) // (maximum + 1)
-        # print(f'Bucket index: {bucket_index}')
-        # print(bucket_index)
         buckets[bucket_index].append(number)
-    # print(f'Buckets before sorting: {buckets}')
-    # TODO: Sort each bucket using any sorting algorithm (recursive or another)
+    # Sort each bucket using any sorting algorithm (recursive or another)
     for index, bucket in enumerate(buckets):
         if bucket.size > 0:
             values = bucket.items()
             merge_sort(values)
             buckets[index] = LinkedList(values)
-    # print(f'Buckets after sorting: {buckets}')
-    # TODO: Loop over buckets and append each bucket's numbers into output list
+    # Loop over buckets and append each bucket's numbers into output list
     numbers_index = 0
     for index, bucket in enumerate(buckets):
         b_size = bucket.size
